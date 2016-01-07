1160667e4d2899cde19538a4afb4ccc5cd7e8831
Deivid Azevedo <deividazevedo1@gmail.com>
2016-01-06 22:53:09 -0300

    ,

--- src/main/java/br/com/cosmetics/entities/Pessoa.java
+++ src/main/java/br/com/cosmetics/entities/Pessoa.java
@@ -4,4 +4,12 @@ import java.io.Serializable;
 
 public class Pessoa implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3115310905655198212L;
+
+	public Pessoa() {
+	}
+
 }
